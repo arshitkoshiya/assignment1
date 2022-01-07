2d17bb5ec242b655dc29edee8ded867db7556198
arshitkoshiya <97168905+arshitkoshiya@users.noreply.github.com>
2022-01-07 12:43:21 +0530

    now upadated

--- assignment1.js
+++ assignment1.js
@@ -1,2 +1,33 @@
-document.write("hello world");
-document.write("i am AK");
+
+               function calculate(operation){
+                    var first = parseInt(document.getElementById('first').value);
+                    var second = parseInt(document.getElementById('second').value);
+                    var ans;
+                    switch (operation) {
+                         case "+":
+                              ans = first + second;
+                              break;
+                    
+                         case "-":
+                              ans = first - second;
+                              break;
+                    
+                         case "*":
+                              ans = first * second;
+                              break;
+                    
+                         case "/":
+                              if(first != 0 && second != 0){
+                                   ans = first / second;
+                              }else{
+                                   ans = 'Please enter correct value';
+                              }
+                              break;
+                    
+                         default:
+                              ans ="error";
+                              break;
+                    }
+                    var setAns = document.getElementById('answer');
+                    setAns.value = ans;
+               }
